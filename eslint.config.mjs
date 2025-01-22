bd8b04e548cafc194ef9c587add06073f8c6351c
胡书康 <hushukang@gmail.com>
2025-01-22 19:26:46 +0900

    add eslint prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+import prettierConfig from 'eslint-plugin-prettier/recommended';
+
+export default withNuxt({
+  ...prettierConfig,
+});
